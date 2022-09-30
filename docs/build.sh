635eca4a366d6a2b4883ebd151ad86689aa6644b
Thomas Gurriet <tgurriet@3laws.io>
2022-09-30 04:06:59 +0200

    wip doc script

--- docs/build.sh
+++ docs/build.sh
@@ -1,16 +1,23 @@
 #!/bin/bash
 
-declare -a versions=("v0.1.0" "v0.2.0")
+declare -A versions=()
+versions["refs/tags/v0.1.0"]="0.1.0"
+versions["refs/tags/v0.2.0"]="0.2.0"
+versions["test"]="latest"
 
-# git clone --single-branch --bare --branch master git@github.com:3LawsRobotics/3laws.git src
+git clone --single-branch --bare --branch master git@github.com:3LawsRobotics/3laws.git src
 mkdir -p gh-pages
 cd src
 
-for v in "${versions[@]}"
+for branch in "${!versions[@]}"
 do
-  git fetch origin "refs/tags/$v":"refs/tags/$v"
-  git worktree add -f "../$v" "refs/tags/$v"
-  sphinx-build -W -b html -d $v/_build/.doctrees ../$v/docs/ ../gh-pages/en/$v
+  folder="${versions[$branch]}"
+  git fetch origin $branch:$branch
+  git worktree add -f "../$folder" $branch
+  sphinx-build -W -b html -d ../$folder/_build/.doctrees ../$folder/docs/ ../gh-pages/en/$folder
+  # sphinx-build -W -b latex -d ../$folder/_build/.doctrees ../$folder/docs/ ../$folder/_build/latex
+  # make -C ../$folder/_build/latex all-pdf
+  # cp ../$folder/_build/latex/3laws.pdf ../gh-pages/en/$folder/3laws.pdf
 done
 
-# sphinx-build -b html . html
+cp ../latest/docs/index.html ../gh-pages
