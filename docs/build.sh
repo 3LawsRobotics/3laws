0e3944883c9f0593131df9e62ea629914fb4adcc
Thomas Gurriet <tgurriet@3laws.io>
2022-09-30 03:38:36 +0200

    wip

--- docs/build.sh
+++ docs/build.sh
@@ -9,8 +9,8 @@ cd src
 for v in "${versions[@]}"
 do
   git fetch origin "refs/tags/$v":"refs/tags/$v"
-  git worktree add "../$v" "refs/tags/$v"
-  # sphinx-build -W -b html -d $v/_build/.doctrees ../$v/ ../gh-pages/en/$v
+  git worktree add -f "../$v" "refs/tags/$v"
+  sphinx-build -W -b html -d $v/_build/.doctrees ../$v/docs/ ../gh-pages/en/$v
 done
 
 # sphinx-build -b html . html
