8b01f4415aa783a782f10d2692e9e6de362d9e50
Thomas Gurriet <tgurriet@3laws.io>
2022-09-30 03:33:12 +0200

    wip

--- docs/build.sh
+++ docs/build.sh
@@ -1,3 +1,16 @@
 #!/bin/bash
 
-sphinx-build -b html . html
+declare -a versions=("v0.1.0" "v0.2.0")
+
+# git clone --single-branch --bare --branch master git@github.com:3LawsRobotics/3laws.git src
+mkdir -p gh-pages
+cd src
+
+for v in "${versions[@]}"
+do
+  git fetch origin "refs/tags/$v":"refs/tags/$v"
+  git worktree add "../$v" "refs/tags/$v"
+  # sphinx-build -W -b html -d $v/_build/.doctrees ../$v/ ../gh-pages/en/$v
+done
+
+# sphinx-build -b html . html
