09b7a6d81935ab9e2b711b9e45d0f54f7e0529a8
Thomas Gurriet <tgurriet@3laws.io>
2022-09-30 18:44:19 +0200

    wip

--- docs/build.sh
+++ docs/build.sh
@@ -15,9 +15,9 @@ do
   git fetch origin $branch:$branch
   git worktree add -f "../$folder" $branch
   sphinx-build -W -b html -d ../$folder/_build/.doctrees ../$folder/docs/ ../gh-pages/en/$folder
-  # sphinx-build -W -b latex -d ../$folder/_build/.doctrees ../$folder/docs/ ../$folder/_build/latex
-  # make -C ../$folder/_build/latex all-pdf
-  # cp ../$folder/_build/latex/3laws.pdf ../gh-pages/en/$folder/3laws.pdf
+  sphinx-build -W -b latex -d ../$folder/_build/.doctrees ../$folder/docs/ ../$folder/_build/latex
+  make -C ../$folder/_build/latex all-pdf
+  cp ../$folder/_build/latex/3laws.pdf ../gh-pages/en/$folder/3laws.pdf
 done
 
 cp ../latest/docs/index.html ../gh-pages
--- docs/conf.py
+++ docs/conf.py
@@ -6,6 +6,9 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
+import json
+import os
+
 project = '3Laws'
 copyright = '2022, 3Laws Robotics Inc'
 author = '3Laws Robotics Inc'
@@ -13,7 +16,24 @@ author = '3Laws Robotics Inc'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = [
+    'sphinx.ext.todo',
+    'sphinxcontrib.spelling',
+    'sphinx_sitemap',
+    'notfound.extension',
+]
+
+version = "0.2.0"
+dir_path = os.path.dirname(os.path.realpath(__file__))
+if not os.path.exists(os.path.join(dir_path, "versions.json")):
+    data = {"master": version}
+else:
+    with open("versions.json") as f:
+        json_data = f.read()
+        data = json.loads(json_data)
+versions_dict = data
+
+html_baseurl = "https://3lawsrobotics.github.io/3laws/"
 
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
@@ -21,6 +41,14 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
-
+html_context = {
+    "versions": versions_dict,
+    "current_version": version,
+    "display_github": True,  # Integrate GitHub
+    "github_user": "3LawsRobotics",  # Username
+    "github_repo": "3laws",  # Repo name
+    "github_version": "master",  # Version
+    "conf_py_path": "/docs",  # Path in the checkout to the docs root
+}
 html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
